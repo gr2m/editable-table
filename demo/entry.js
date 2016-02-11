0684bb3e10f40eb3a3138765a16c8084f04a5719
Gregor Martynus <gregor@martynus.net>
2016-02-11 15:42:57 -0500

    fix: enable navigable-table in demo

--- demo/entry.js
+++ demo/entry.js
@@ -4,11 +4,11 @@ require('./demo.css')
 
 var jQuery = require('jquery')
 var editableTable = require('../editable-table')
-// var navigableTable = require('navigable-table')
+var navigableTable = require('navigable-table')
 var expandableInput = require('expandable-input')
 
 expandableInput(jQuery)
 editableTable(jQuery)
-// navigableTable(jQuery)
+navigableTable(jQuery)
 
 window.$ = window.jQuery = jQuery
